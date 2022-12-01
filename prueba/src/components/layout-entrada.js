1d879fd7569cb7aa0e4122e17f09194c76c3227f
Jonathan <you@example.com>
2022-12-01 11:45:19 -0500

    add-funcionalidad.agregarTarea

--- prueba/src/components/layout-entrada.js
+++ prueba/src/components/layout-entrada.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Entrada() {
+
+    const [nuevaTarea, setNuevaTarea] = useState()
 
     return (
         <>
             <center>
-                <input type="text" placeHolder='Título' style={{ fontSize: "22px" }} />
-                <button style={
-                    {
-                        backgroundColor: "#87CEEB",
-                        fontSize: "21px", color: "white",
-                        padding: "4px 18px"
+                <input type="text" placeHolder='Título'
 
-                    }}>Agregar</button>
+                    onChange={(e) => setNuevaTarea(e.target.value)}
+
+                    style={{ fontSize: "22px" }} />
+
+                <button onClick={() => alert(nuevaTarea)}
+                    style={
+                        {
+                            backgroundColor: "#87CEEB",
+                            fontSize: "21px", color: "white",
+                            padding: "4px 18px"
+
+                        }}>Agregar</button>
             </center>
         </>
     )
