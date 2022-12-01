2b32128bf47f9bcc0e111f5b34de6952df48473e
Jonathan <you@example.com>
2022-12-01 10:56:14 -0500

    add-diseño

---
+++ prueba/src/components/layout-a.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Entrada() {
+
+    return (
+        <>
+            <center>
+                <input type="text" placeHolder='Titulo' style={{ fontSize: "20px" }}/>
+                <button style={{ backgroundColor: "#87CEEB", fontSize: "25px" }}>Agregar</button>
+            </center>
+        </>
+    )
+
+}
+
+export default Entrada;
---
+++ prueba/src/components/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Encabezado(){
+
+
+    return(
+        <center><h3 style={{ color: "#87CEEB", fontSize: "35px" }}>TODO LIST</h3></center>
+    )
+
+}
+
+export default Encabezado;
